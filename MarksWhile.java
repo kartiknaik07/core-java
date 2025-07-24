9a95c035382f5168aa76b866ea0bdcfd534ed9a8
kartiknaik07 <kartiknaik353@gmail.com>
2025-07-24 11:37:47 +0530

    Creating a java file

---
+++ MarksWhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class MarksWhile{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+			
+		int i =1;
+		while(i<=5){
+			System.out.print("Enter marks of student" + i + ": ");
+			int marks = sc.nextInt();
+			i++;
+			if(marks>=80 && marks<=100){
+				System.out.println("A");
+			}
+			else if(marks>=60 && marks<80){
+				System.out.println("B");
+			}
+			else if(marks>=35 && marks<60){
+				System.out.println("C");
+			}
+			else if(marks>=0 && marks<35){
+				System.out.println("F");
+			}
+			else{
+				System.out.println("Invalid marks");
+			}
+		}
+	}
+}
